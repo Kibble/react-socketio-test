e720ebbda7a4a65ce0ff93a4e7323f90fe1f9e02
Kibble <kibble.bytes08@gmail.com>
2020-12-25 18:58:40 -0500

    maybe...

---
+++ server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/build/index.html';
+
+// const app = express(); // create express app
+
+// // add middleware
+// app.use(express.static("public"));
+
+// // start express server on port 5000
+// app.listen(5000, () => {
+//   console.log("server started on port 5000");
+// });
+
+const app = express()
+    .use(express.static('build'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(app);
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+//   });
+
+// const server = express()
+// //   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .use((req, res) => res.sendFile(path.join(__dirname, "public", 'index.html')))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+io.on('event', (socket) => {
+    console.log('asdfasdfasdf');
+});
+
+io.on('action', (socket) => {
+    console.log('fdasfdas');
+})
+
+// socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+//     console.log(NEW_CHAT_MESSAGE_EVENT + ': ');
+//     console.log(data);  
+//     io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+//   });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import useChat from './useChat';
+// var io = require('socket.io')(http);
+// import Server from 'socket.io';
 
-function App() {
-  return (
+// import socketIo from 'socket.io';
+
+// const click = () => {
+//   console.log('clicked');
+//   // let io = new Server('http'); 
+//   // io.sockets.emit('action', 'payload');
+//   const { messages, sendMessage } = useChat(12345);
+//   sendMessage('test message');
+//   // useChat.sendMessage('test message');
+// }
+
+const App = () => {
+  const { messages, sendMessage } = useChat(12345);
+  
+  const click = () => {
+    console.log('clicked');
+    // let io = new Server('http'); 
+    // io.sockets.emit('action', 'payload');
+    
+    sendMessage('test message');
+  }
+
+  return (  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <a className="App-link" href="#" onClick={click}>Test</a>
+
         <a
           className="App-link"
           href="https://reactjs.org"
